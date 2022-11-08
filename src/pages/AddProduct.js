adf88c5e99bba9ba3e8b0117c4e75cb8f5e718d8
takdirhossain <takdirhossain35@gmail.com>
2022-11-08 12:42:27 +0600

    add service

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,38 +1,36 @@
 import React from 'react';
 
 const AddProduct = () => {
-    return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
+  return (
+    <div className="container mx-auto px-4 sm:px-6 md:px-0">
+      <div className="flex items-center justify-center mt-36 mb-36">
+        <div data-aos="fade-up" data-aos-duration="1000" className=' md:w-[48%] lg:w-2/6 px-4 py-5 rounded shadow hover:shadow-md'>
+          <h3 className="text-3xl text-center mb-3">Add Product</h3>
+          <form action="">
+            <div className="mb-2">
+            <input type="text"  placeholder="Service Name"  name='service-price' className="input input-bordered  w-full py-2 px-3 resize-none rounded-sm" />
             </div>
-          </div>
+
+            <div className="mb-2">
+            <input type="text"  placeholder="Service price"  name='service-price' className="input input-bordered  w-full py-2 px-3 resize-none rounded-sm" />
+
+            </div>
+            
+            
+            <div className="mb-1">
+              
+              <textarea className="textarea textarea-bordered w-full py-2 px-3 resize-none rounded-sm" placeholder="Description"></textarea>
+
+            </div>
+            <div className="mb-2">
+            <textarea className="textarea textarea-bordered w-full py-2 px-3 resize-none rounded-sm" placeholder="Description"></textarea>
+            </div>
+            <button className="btn bg-blue w-full" to='/'> Add SErvice </button>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default AddProduct;
