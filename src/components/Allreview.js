41120ff08674533cfe69effeba9c1c36de0bc730
takdirhossain <takdirhossain35@gmail.com>
2022-11-10 01:10:21 +0600

    show review

---
+++ src/components/Allreview.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Allreview = ({ rev }) => {
+    console.log(rev)
+    const { name, photo, review, } = rev
+    return (
+        
+            <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={photo} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{name}</div>
+
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {review}
+                                <br />
+
+                            </td>
+
+                            <th>
+                                <button className="btn btn-ghost btn-xs">Update</button>
+                            </th>
+                        </tr>
+        
+    );
+};
+
+export default Allreview;
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,26 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { version } from 'react-dom';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import Allreview from '../components/Allreview';
 import { MainContext } from '../contex/AuthContex';
 
 const Service = () => {
     const { user } = useContext(MainContext)
     const data = useLoaderData()
+    const [reviewed, setReviewed] = useState([])
     const { name, img, description, _id } = data
 
     const handaleSubmit = event => {
         event.preventDefault()
+        const form = event.target
         const review = event.target.review.value
         const name = user?.displayName
         const photo = user?.photoURL
-        const addreview = {
-            service: _id,
-             name,
-            review,
-           
-            photo,
-        }
-        console.log(addreview)
+        const addreview = { service: _id, name, review, photo, }
+
+     
         fetch(`http://localhost:5000/review`, {
             method: "POST",
             headers: {
@@ -28,12 +27,31 @@ const Service = () => {
             },
             body: JSON.stringify(addreview)
         })
-        .then(res => res.json())
-        .then(data => console.log(data))
-       
+            .then(res => res.json())
+            .then(data => {
+                form.reset()
+                Swal.fire(
+                    'Good job!',
+                    'You clicked the button!',
+                    'success'
+                  )
+            })
+
     }
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+        .then(res => res.json())
+        .then(data => {
+            const newReview = data.filter(rv => {
+                return rv.service === _id
+            })
+            setReviewed(newReview)
+           
+        })
+    }, [_id, reviewed])
     return (
-        <div className="card card-side bg-base-100 shadow-xl grid md:grid-cols-1 lg:grid-cols-2 p-3 mt-28 mb-28">
+       <div>
+         <div className="card card-side bg-base-100 shadow-xl grid md:grid-cols-1 lg:grid-cols-2 p-3 mt-28 mb-28">
             <img className='rounded' src={img} alt="Movie" />
             <div className="card-body">
                 <div className='flex justify-start	'>
@@ -52,6 +70,40 @@ const Service = () => {
                 </div>
             </div>
         </div>
+        <div>
+        <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                       
+
+
+                    </tbody>
+
+        {
+            reviewed.map(rev => <Allreview key={rev._id} rev={rev}></Allreview> )
+        }
+
+
+                </table>
+            </div>
+        </div>
+        
+       </div>
     );
 };
 
