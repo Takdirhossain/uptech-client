9d4b025fdcea5c70b8d51568274be40dc912592b
takdirhossain <takdirhossain35@gmail.com>
2022-11-10 19:37:19 +0600

    set banner

--- src/components/Trande.js
+++ src/components/Trande.js
@@ -1,22 +1,27 @@
 import React from 'react';
-
+import background from '../assets/app-bannerimg.png'
 const Trande = () => {
     return (
         <div>
-            <h2 className='text-6xl font-bold text-center'>latest Technology</h2>
+            <h2 className='text-6xl font-bold text-center'>Teande Technology</h2>
             <div className="hero  latest py-20">
                 <div className="hero-content flex flex-col lg:flex-row">
                     <div className='w-2/3 '>
 
-                        <img src='/' className="   " alt='' />
+                        <img src={background} className="   " alt='' />
 
                     </div>
-                    <div className='w-2/3'>
+                    <div className='w-2/3 ml-20'>
 
                         <label className='banner-title  bg-blue p-2 rounded-full'>LATEST TECHNOLOGIES</label>
-                        <h1 className="text-6xl mt-5 font-bold">All your needs <span className='text-6xl font-normal'>just <br /> in one app</span> </h1>
+                        <h1 className="text-6xl mt-5 font-bold">Trend   <span className='text-6xl font-normal'>app with it’s easy ecosystem</span> </h1>
                         <p className="py-6 mt-5 mr-3 ">  Pellentesque at posuere tellus. Ut sed dui justo. Phasellus scelerisque turpis arcu, ut pulvinar lectus tristique non. ia faucibus nunc, vel pellentesque nibh condimentum non.  </p>
-                        <button className="btn bg-blue">Read More</button>
+                        <progress className="progress progress-secondary w-56" value="0" max="100"></progress><br />
+                        <progress className="progress progress-secondary w-56" value="10" max="100"></progress><br />
+                        <progress className="progress progress-secondary w-56" value="40" max="100"></progress><br />
+                        <progress className="progress progress-secondary w-56" value="70" max="100"></progress><br />
+                        <progress className="progress progress-secondary w-56" value="100" max="100"></progress>
+                       
                     </div>
 
                 </div>
